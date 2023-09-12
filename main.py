95ae462a86accd54160490639d14dc05aa439156
DimaHmel <144783394+DimaHmel@users.noreply.github.com>
2023-09-12 13:04:25 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+print('dbnmgyu')
+name='sdfghr'
+
+
+
+a=int(input('Введите числа a:'))
+b=int(input('Введите числа b:'))
+c=int(input('Введите числа c:'))
+if(a+b>c and a+c>b and b+c>a):
+    print('есть')
